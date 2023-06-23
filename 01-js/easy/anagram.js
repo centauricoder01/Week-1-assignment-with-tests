d584a35633934e738424e8c08b6f51bb1728256d
Rajendra <itsrajendrapatel@gmail.com>
2023-06-23 09:50:14 +0530

    First Question Solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,22 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase().split("").sort();
+  str2 = str2.toLowerCase().split("").sort();
 
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
+console.log(isAnagram("ra@ju", "Raju@"));
 
 module.exports = isAnagram;
